31fa3ed82d93a5897598a04b9eb5f9539a975873
Shun Yao <ysmiracle@hotmail.com>
2018-03-31 09:09:17 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -24,28 +24,27 @@ def play(songs)
   if input == "1" || input == "Phoenix - 1901"
     puts "Playing #{songs[0]}"
   elsif  input == "2" || input == "Tokyo Police Club - Wait Up"
-    puts "#{songs[1]}"
+    puts "Playing #{songs[1]}"
   elsif  input == "3" || input == "Sufjan Stevens - Too Much"
-    puts "#{songs[2]}"
+    puts "Playing #{songs[2]}"
   elsif input == "4" || input == "The Naked and the Famous - Young Blood"
-    puts "#{songs[3]}"
+    puts "Playing #{songs[3]}"
   elsif input == "5" || input == "(Far From) Home - Tiga"
-    puts "#{songs[4]}"
+    puts "Playing #{songs[4]}"
   elsif input == "6" || input == "The Cults - Abducted"
-    puts "#{songs[5]}"
+    puts "Playing #{songs[5]}"
   elsif input == "7" || input == "Phoenix - Consolation Prizes"
-    puts "#{songs[6]}"
+    puts "Playing #{songs[6]}"
   elsif input == "8" || input == "Harry Chapin - Cats in the Cradle"
-    puts "#{songs[7]}"  
+    puts "Playing #{songs[7]}"  
   elsif input == "9" || input == "Amos Lee - Keep It Loose, Keep It Tight"
-    puts "#{songs[8]}"
+    puts "Playing #{songs[8]}"
   else
     puts "Invalid input, please try again"
   end
 end
       
 def list(songs)
-  #puts "Phoenix - 1901"
   songs.each_with_index do |val, index|
     puts "#{index+1}. #{val}"
   end
